7717da2f8656292f08a9d1f5b7a76d3fb9f6c52a
faizan72560 <faizanahmad72560@gmail.com>
2024-08-16 18:35:16 +0530

    commented one

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -12,11 +12,11 @@ const Main=()=>{
                 <div className="text-[#fa6e01] text-3xl mt-[10px]">
                 Software Developer
                 </div>
-                <div className="flex mt-[20px]">
+                {/* <div className="flex mt-[20px]">
                     <button className='text-bold h-[30px] w-[80px] bg-[#fa6e01] text-white rounded-[7px] '>Hire Me</button>
                     <button className='text-bold h-[30px] w-[120px] rounded-[10px] border border-grey-700 text-white rounded-[7px] mx-2'>DownLoad Cv</button>
 
-                </div>
+                </div> */}
                 <div className="bg-[#1b1b1b] h-[70px] w-[300px] mt-[30px] rounded-[7px] flex items-center justify-center">
                     <div className="flex flex-col justify-center">
                         <div className="text-[#fa6e01] flex flex-col items-center"> 
